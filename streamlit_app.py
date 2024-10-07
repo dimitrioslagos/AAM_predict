c51f4c0e9d93b433c29f268f2a2571a3bfa53855
dimitrioslagos <lagosdemetres@gmail.com>
2024-10-07 14:19:51 +0300

    Update streamlit_app.py
    same as previous

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import math
 from pathlib import Path
-from AAM_predict_toolbox import compute_warning_on_bushing, compute_warning_on_DGA
+from AAM_predict_toolbox import compute_warning_on_bushing, compute_warning_on_DGA,display_light
 
 
 # Set the title of the Streamlit app
