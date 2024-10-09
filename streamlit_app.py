e1a55a2de2a1d1f06aa5a4efde7c5a0826b60e8f
Dimitris <dimitrios_lagos@hotmail.com>
2024-10-09 09:32:01 +0300

    Update app to present future oil plot & and train current models - v5 (update html plot height in tab3)

--- streamlit_app.py
+++ streamlit_app.py
@@ -137,4 +137,4 @@ with tabs[2]:
     if ('model_oil' in st.session_state)&('current_models' in st.session_state):
         t = pd.to_datetime('2024-06-13 08:00:00')
         OIL_temp = predict_oil_future(model_oil, current_models, OLMS_DATA, t)
-        st.components.v1.html(html_future_oil_temp_plot(OIL_temp))
+        st.components.v1.html(html_future_oil_temp_plot(OIL_temp),height=600)
